bc8a1fe148a3081b178151419c588bba51ec3a20
Chris <christian.valenzuela@correounivalle.edu.co>
2023-11-05 10:54:34 -0500

    Part1 Complete
    Solve questions 1 - 5 and run test successful
    	modified:   part1/question1.py
    	modified:   part1/question2.py
    	modified:   part1/question3.py
    	modified:   part1/question3_test.py
    	modified:   part1/question4.py
    	modified:   part1/question5.py
    	new file:   part1/quiz_pets

--- part1/question3.py
+++ part1/question3.py
@@ -1,44 +1,74 @@
 ################################################################################
 #     ____                          __     _                          _____
 #    / __ \  __  __  ___    _____  / /_   (_)  ____    ____          |__  /
-#   / / / / / / / / / _ \  / ___/ / __/  / /  / __ \  / __ \          /_ < 
-#  / /_/ / / /_/ / /  __/ (__  ) / /_   / /  / /_/ / / / / /        ___/ / 
-#  \___\_\ \__,_/  \___/ /____/  \__/  /_/   \____/ /_/ /_/        /____/  
-#                                                                          
+#   / / / / / / / / / _ \  / ___/ / __/  / /  / __ \  / __ \          /_ <
+#  / /_/ / / /_/ / /  __/ (__  ) / /_   / /  / /_/ / / / / /        ___/ /
+#  \___\_\ \__,_/  \___/ /____/  \__/  /_/   \____/ /_/ /_/        /____/
+#
 #  Question 3
 ################################################################################
 #
 # Instructions:
-# Make a Python class for a magical oven that can combine ingredients at 
+# Make a Python class for a magical oven that can combine ingredients at
 # different temperatures to craft special materials.
-# 
+#
 # The oven class should have the methods:
 # - add(item) to add an oven to be combined
 # - freeze() to freeze the ingredients
 # - boil() to boil the ingredients
 # - wait() to combine the ingredients with no change in temperature
-# - get_output() to get the result 
+# - get_output() to get the result
 #
 # You will need to change the `make_oven()` function to return a new instance
 # of your oven.
 #
-# The `alchemy_combine()` function will use your oven. You can see the expected 
+# The `alchemy_combine()` function will use your oven. You can see the expected
 # formulas and their outputs in the test file, `question3_test.py`.
+
+
+class MagicalOven:
+    def __init__(self):
+        self.ingredients = []
+        self.temperature = 0
+        self.output = None
+
+    def add(self, item):
+        self.ingredients.append(item)
+
+    def freeze(self):
+        self.temperature = -100
+
+    def boil(self):
+        self.temperature = 100
+
+    def wait(self):
+        self.temperature = 0
+
+    def get_output(self):
+        if self.ingredients == ["lead", "mercury"] and self.temperature == 100:
+            self.output = "gold"
+        elif self.ingredients == ["water", "air"] and self.temperature == -100:
+            self.output = "snow"
+        elif self.ingredients == ["cheese", "dough", "tomato"] and self.temperature == 100:
+            self.output = "pizza"
+        return self.output
+
 
 # This function should return an oven instance!
 def make_oven():
-  None
+    return MagicalOven()
+
 
 def alchemy_combine(oven, ingredients, temperature):
-  
-  for item in ingredients:
-    oven.add(item)
 
-  if temperature < 0:
-    oven.freeze()
-  elif temperature >= 100:
-    oven.boil()
-  else:
-    oven.wait()
+    for item in ingredients:
+        oven.add(item)
 
-  return oven.get_output()
+    if temperature < 0:
+        oven.freeze()
+    elif temperature >= 100:
+        oven.boil()
+    else:
+        oven.wait()
+
+    return oven.get_output()
--- part1/question5.py
+++ part1/question5.py
@@ -1,15 +1,15 @@
 ################################################################################
 #     ____                          __     _                           ______
 #    / __ \  __  __  ___    _____  / /_   (_)  ____    ____           / ____/
-#   / / / / / / / / / _ \  / ___/ / __/  / /  / __ \  / __ \         /___ \  
-#  / /_/ / / /_/ / /  __/ (__  ) / /_   / /  / /_/ / / / / /        ____/ /  
-#  \___\_\ \__,_/  \___/ /____/  \__/  /_/   \____/ /_/ /_/        /_____/   
-#                                                                            
+#   / / / / / / / / / _ \  / ___/ / __/  / /  / __ \  / __ \         /___ \
+#  / /_/ / / /_/ / /  __/ (__  ) / /_   / /  / /_/ / / / / /        ____/ /
+#  \___\_\ \__,_/  \___/ /____/  \__/  /_/   \____/ /_/ /_/        /_____/
+#
 #  Question 5
 ################################################################################
 #
 # Instructions:
-# This questions continues to use the database we worked with in Question 4. In 
+# This questions continues to use the database we worked with in Question 4. In
 # this question, we will made some modifications ot the table.
 
 # Part 5.A:
@@ -19,19 +19,24 @@
 # vegetarian integer
 
 sql_create_favorite_foods = """
-
-Your SQL here.
+CREATE TABLE favorite_foods (
+    food_id INTEGER PRIMARY KEY,
+    name TEXT,
+    vegetarian INTEGER
+);
 
 """
 
 # Part 5.B:
 # Alter the animals and people tables by adding a new column to each called 'favorite_food_id'
-# The test suite will verify the new changes by inserting some new rows. 
+# The test suite will verify the new changes by inserting some new rows.
 
 sql_alter_tables_with_favorite_food = """
+ALTER TABLE animals
+ADD COLUMN favorite_food_id INTEGER;
 
-Your SQL here.
-
+ALTER TABLE people
+ADD COLUMN favorite_food_id INTEGER;
 """
 
 # Part 5.C:
@@ -39,7 +44,8 @@ Your SQL here.
 # THe output should be a list of tuples in the format: (<pet name>, <food name>)
 
 sql_select_all_vegetarian_pets = """
-
-Your SQL here.
-
+SELECT a.name, ff.name
+FROM animals AS a
+JOIN favorite_foods AS ff ON a.favorite_food_id = ff.food_id
+WHERE ff.vegetarian = 1;
 """
